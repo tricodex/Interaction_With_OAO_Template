4e7327d54386a73c3f1708c7bd60cdab93c2b4ba
hadzija7 <hadzijasale@gmail.com>
2024-04-09 13:49:18 +0200

    added TestPrompt.sol which exposes methods for testing

---
+++ src/test/TestPrompt.sol
@@ -0,0 +1,102 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "../interfaces/IAIOracle.sol";
+import "../AIOracleCallbackReceiver.sol";
+
+/// @notice User interfacing contract that interacts with OAO
+/// @author ora.io
+/// @dev Prompt contract inherits AIOracleCallbackReceiver, so that OPML nodes can callback with the result.
+contract TestPrompt is AIOracleCallbackReceiver {
+    
+    event promptsUpdated(
+        uint256 requestId,
+        uint256 modelId,
+        string input,
+        string output,
+        bytes callbackData
+    );
+
+    event promptRequest(
+        uint256 requestId,
+        address sender, 
+        uint256 modelId,
+        string prompt
+    );
+
+    struct AIOracleRequest {
+        address sender;
+        uint256 modelId;
+        bytes input;
+        bytes output;
+    }
+
+    address public owner;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner");
+        _;
+    }
+
+    /// @dev requestId => AIOracleRequest
+    mapping(uint256 => AIOracleRequest) public requests;
+
+    /// @dev modelId => callback gasLimit
+    mapping(uint256 => uint64) public callbackGasLimit;
+
+    /// @notice Initialize the contract, binding it to a specified AIOracle.
+    constructor(IAIOracle _aiOracle) AIOracleCallbackReceiver(_aiOracle) {
+        owner = msg.sender;
+        callbackGasLimit[50] = 500_000; // SD 500k
+        callbackGasLimit[11] = 5_000_000; // llama
+        callbackGasLimit[9] = 5_000_000; // grok
+    }
+
+    /// @notice sets the callback gas limit for a model
+    /// @dev only only can set the gas limit
+    function setCallbackGasLimit(uint256 modelId, uint64 gasLimit) external onlyOwner {
+        callbackGasLimit[modelId] = gasLimit;
+    }
+
+    /// @dev uint256: modelID => (string: prompt => string: output)
+    mapping(uint256 => mapping(string => string)) public prompts;
+
+    /// @notice returns the output for a specific model and prompt
+    function getAIResult(uint256 modelId, string calldata prompt) external view returns (string memory) {
+        return prompts[modelId][prompt];
+    }
+
+    /// @notice OAO executes this method after it finishes with computation
+    /// @param requestId requestId 
+    /// @param output result of the OAO computation
+    /// @param callbackData optional data that is executed in the callback
+    function aiOracleCallback(uint256 requestId, bytes calldata output, bytes calldata callbackData) external override onlyAIOracleCallback() {
+        // since we do not set the callbackData in this example, the callbackData should be empty
+        AIOracleRequest storage request = requests[requestId];
+        require(request.sender != address(0), "request does not exist");
+        request.output = output;
+        prompts[request.modelId][string(request.input)] = string(output);
+        emit promptsUpdated(requestId, request.modelId, string(request.input), string(output), callbackData);
+    }
+
+    /// @notice estimating fee that is spent by OAO
+    function estimateFee(uint256 modelId) public view returns (uint256) {
+        return aiOracle.estimateFee(modelId, callbackGasLimit[modelId]);
+    }
+
+    /// @notice main point of interaction with OAO
+    /// @dev aiOracle.requestCallback sends request to OAO
+    function calculateAIResult(uint256 modelId, string calldata prompt) payable external returns (uint256) {
+        bytes memory input = bytes(prompt);
+        // we do not need to set the callbackData in this example
+        uint256 requestId = aiOracle.requestCallback{value: msg.value}(
+            modelId, input, address(this), callbackGasLimit[modelId], ""
+        );
+        AIOracleRequest storage request = requests[requestId];
+        request.input = input;
+        request.sender = msg.sender;
+        request.modelId = modelId;
+        emit promptRequest(requestId, msg.sender, modelId, prompt);
+        return requestId;
+    }
+}
--- test/Prompt.t.sol
+++ test/Prompt.t.sol
@@ -2,8 +2,8 @@
 pragma solidity ^0.8.13;
 
 import {Test, console2, Vm} from "forge-std/Test.sol";
-import {Prompt} from "../src/Prompt.sol";
-import {AIOracle} from "../src/AIOracle.sol";
+import {TestPrompt} from "../src/test/TestPrompt.sol";
+import {IAIOracle} from "../src/interfaces/IAIOracle.sol";
 import {OraSepoliaAddresses} from "./OraSepoliaAddresses.t.sol";
 import "forge-std/console.sol";
 
@@ -32,16 +32,6 @@ contract PromptTest is Test, OraSepoliaAddresses {
         string prompt
     );
 
-    event AICallbackRequest(
-        address indexed account,
-        uint256 indexed requestId,
-        uint256 modelId,
-        bytes input,
-        address callbackContract,
-        uint64 gasLimit,
-        bytes callbackData
-    );
-
     event promptsUpdated(
         uint256 requestId,
         uint256 modelId,
@@ -50,25 +40,19 @@ contract PromptTest is Test, OraSepoliaAddresses {
         bytes callbackData
     );
 
-    Prompt prompt;
-    AIOracle aiOracle;
-    uint256 modelId;
-    string input;
-    string sepoliaRpc;
+    TestPrompt prompt;
+    string rpc;
     uint256 forkId;
 
     function setUp() public {
-        sepoliaRpc = vm.envString("SEPOLIA_RPC");
-        forkId = vm.createSelectFork(sepoliaRpc);
-
-        prompt = new Prompt(AIOracle(OAO_PROXY));
-        modelId = 11; //llama
-        input = "What is a good use case for on-chain AI?";
+        rpc = vm.envString("SEPOLIA_RPC");
+        forkId = vm.createSelectFork(rpc);
+        prompt = new TestPrompt(IAIOracle(OAO_PROXY));
     }
 
     function test_SetUp() public {
         assertNotEq(address(prompt), address(0));
-        // assertEq(prompt.owner(), address(this));
+        assertEq(prompt.owner(), address(this));
         assertEq(address(prompt.aiOracle()), OAO_PROXY);
         assertEq(prompt.callbackGasLimit(STABLE_DIFUSION_ID), 500_000);
         assertEq(prompt.callbackGasLimit(LLAMA_ID), 5_000_000);
@@ -92,18 +76,17 @@ contract PromptTest is Test, OraSepoliaAddresses {
     }
 
     function test_OAOInteraction() public {
+        vm.expectRevert("insufficient fee");
+        prompt.calculateAIResult(11, "What is a good use case for on-chain AI?");
+
         vm.expectEmit(false, false, false, false);
-        //check if requestCallback method is executed
-        emit promptRequest(3355, address(this), modelId, input);
-        //check if OPML node called aiOracleCallback method
-        emit promptsUpdated(3355, modelId, input, "", "");
-        prompt.calculateAIResult{value: prompt.estimateFee(11)}(modelId, input);
-
-        //wait for the next block
-
-        // (,uint256 modelId,bytes memory request_input,) = prompt.requests(3357);
-        // string memory output = prompt.getAIResult(modelId, string(request_input));
-        // assertNotEq(output, "");
-        // console.log(output);
+        emit promptRequest(3847, address(this), 11,  "What is a good use case for on-chain AI?");
+        uint256 requestId = prompt.calculateAIResult{value: prompt.estimateFee(11)}(11, "What is a good use case for on-chain AI?");
+        
+        (address sender, uint256 modelId, bytes memory prompt, bytes memory output) = prompt.requests(requestId);
+        assertEq(modelId, 11);
+        assertEq(sender, address(this));
+        assertEq(prompt, "What is a good use case for on-chain AI?");
+        assertEq(string(output), "");
     }
 }
