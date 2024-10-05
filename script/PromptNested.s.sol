021b144ad0601cc516fd559e8b7b8e335adcac27
Patrick C <71180317+tricodex@users.noreply.github.com>
2024-10-05 12:52:59 +0800

    Update PromptNested.s.sol

--- script/PromptNested.s.sol
+++ script/PromptNested.s.sol
@@ -3,19 +3,19 @@ pragma solidity ^0.8.13;
 
 import {Script, console2} from "forge-std/Script.sol";
 import {PromptNestedInference} from "../src/PromptNestedInference.sol";
-import {OraSepoliaAddresses} from "../test/OraSepoliaAddresses.t.sol";
 import {IAIOracle} from "OAO/contracts/interfaces/IAIOracle.sol";
 
-contract PromptScript is Script, OraSepoliaAddresses {
-    function setUp() public {
-        // testnet sepolia address
-        OAO_PROXY = address(0x0A0f4321214BB6C7811dD8a71cF587bdaF03f0A0);
-    }
+contract PromptScript is Script {
+    address public constant OAO_PROXY = 0x0A0f4321214BB6C7811dD8a71cF587bdaF03f0A0;
+
+    function setUp() public {}
 
     function run() public {
-        uint privateKey = vm.envUint("PRIVATE_KEY");
+        uint256 privateKey = vm.envUint("PRIVATE_KEY");
+        
         vm.startBroadcast(privateKey);
-        new PromptNestedInference(IAIOracle(OAO_PROXY));
+        PromptNestedInference promptNestedInference = new PromptNestedInference(IAIOracle(OAO_PROXY));
+        console2.log("PromptNestedInference deployed at:", address(promptNestedInference));
         vm.stopBroadcast();
     }
 }
